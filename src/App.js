1e4594f3b4f930d415788141b94393a434e17308
Geremsa <geremsa111@gmail.com>
2020-11-14 23:20:25 +0530

    queries

--- src/App.js
+++ src/App.js
@@ -4,22 +4,30 @@ import firebase from "./config/base";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 const auth = firebase.auth();
 const firestore = firebase.firestore();
+
+const usersRef = firestore.collection("users");
 const messagesRef = firestore.collection("messages");
 const query = messagesRef.orderBy("createdAt").limit(30);
-const privateQuery = messagesRef.where("uid","==","JvGqfM0J54SHcCeAUGGRvpFeoPx1").where("photoURL","==","https://lh3.googleusercontent.com/a-/AOh14GhM3q7oiK5FKoGI9WHpUECJcNhjtdRzd2bjT6bOiA=s96-c");
+const privateQuery = messagesRef.where("uid","==","JvGqfM0J54SHcCeAUGGRvpFeoPx1").where("photoURL","==","https://lh3.googleusercontent.com/a-/AOh14GhM3q7oiK5FKoGI9WHpUECJcNhjtdRzd2bjT6bOiA=s96-c").orderBy("createdAt");
 
 const FirestoreDocument = () => {
   const [text, settext] = React.useState("");
   const [value, loading, error] = useCollectionData(privateQuery, {
     snapshotListenOptions: { includeMetadataChanges: true },
   });
-  const signIn = () => {
+  const signIn =async () => {
     const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithRedirect(provider);
+    auth.signInWithRedirect(provider).then(async(result)=>{
+      
+    }).catch(err=>console.log(err));
     auth
       .getRedirectResult()
-      .then((result) => console.log(result))
+      .then((result) => {
+        console.log(result);
+       
+      })
       .catch((error) => console.log(error));
+     
   };
   const signOut = () => {
     auth.signOut();
@@ -28,14 +36,17 @@ const FirestoreDocument = () => {
     <div>
       {!auth.currentUser && <button onClick={signIn}>signIn</button>}
       {auth.currentUser && <button onClick={signOut}>signOut</button>}
-      {/* {
+      {
        auth.currentUser &&
-      <button type="submit" onClick={()=>console.log(auth.currentUser)}>Current user</button>
-     } */}
+      <button type="submit" onClick={async()=>await usersRef.add({
+        person:"hb",
+        uid:auth.currentUser.uid
+      })}>Current user</button>
+     }
       {error && <strong>Error: {JSON.stringify(error)}</strong>}
       {loading && <span>Document: Loading...</span>}
       {/* {value && <span>{JSON.stringify(value.data().text)}</span>} */}
-      {value && value.map((p) => <h5 key={p}>{p.text}</h5>)}
+      {value && value.map((p) => <h5 key={p.text}>{p.text}</h5>)}
       <form
         onSubmit={async (e) => {
           e.preventDefault();
@@ -43,7 +54,7 @@ const FirestoreDocument = () => {
           await messagesRef.add({
             text,
             createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-            uid: uid,
+            uid,
             photoURL,
           });
           settext("");
