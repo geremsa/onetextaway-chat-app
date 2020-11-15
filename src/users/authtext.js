f685200730a42b09336aa2764304496b0efddbd1
Geremsa <geremsa111@gmail.com>
2020-11-15 19:43:06 +0530

    logo

--- src/users/authtext.js
+++ src/users/authtext.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './authtext.css'
 
+
 function Authtext() {
     return (
         <section className="auth-container">
         <div className="auth-top">
-
+        <img src="./chat.svg" alt="Chat" className="conversation"/>
         </div>
         <div className="auth-bottom">
             <div className="auth-text">
